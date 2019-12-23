4c7f0f26598a50655945c6e7b5d4704e1abce0be
Richard Allen <richard2891@gmail.com>
2019-12-22 22:02:52 -0500

    added compact solution.

---
+++ compact/compact.py
@@ -0,0 +1,13 @@
+def compact(data):
+    val = []
+    for x in data:
+        if val:
+            if x != val[-1]:
+                val.append(x)
+        else:
+            val.append(x)
+
+    return iter(val)
+
+if __name__ == "__main__":
+    print (compact([ 1, 1, 1]))
