29e6e6e22f993f7bb0c1a7e706c56ba696f12bba
Richard Allen <richard2891@gmail.com>
2019-12-22 22:18:12 -0500

    added tail and count_words solution.

---
+++ count_words/count.py
@@ -0,0 +1,26 @@
+import string
+import re
+
+chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def count_words(sentences):
+    rv = {}
+    #sentences = sentences.translate(str.maketrans('', '', string.punctuation)).lower()
+    #sentences = re.sub(r'[\W_]+', '', sentences.lower())
+    sentences = sentences.lower()
+    lst = sentences.split(' ')
+    for word in lst:
+        if word[0] not in chars:
+            word = word[1:]
+        if word[-1] not in chars:
+            word = word[0:-1]
+        if word in rv:
+            rv[word] += 1
+        else:
+            rv[word] = 1
+    return rv
+
+
+if __name__ == '__main__':
+    count = count_words("oh. what. a day what a lovely day!")
+    print(count)
---
+++ tail/tail.py
@@ -0,0 +1,28 @@
+import types
+
+def tail(elements, n):
+    rv = []
+    if n > 0:
+        if isinstance(elements, types.GeneratorType):
+            elements = [x for x in elements]
+        n *= -1
+        rv = elements[n:len(elements)]
+        if isinstance(rv, str):
+            rv = [char for char in rv]
+        if type(elements) is tuple:
+            return list(rv)
+
+    return rv
+
+
+if __name__ == "__main__":
+    print(tail([1, 2, 3, 4, 5], 3))
+    print(tail('hello', 2))
+    print(tail('hello', 0))
+    print(tail('hello', -2))
+
+    squares = (n ** 2 for n in range(10))
+
+    print(tail(squares,3))
+
+    print(tail((1, 2, 3), 3))
