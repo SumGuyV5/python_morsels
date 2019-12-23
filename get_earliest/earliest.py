a0b9434d0985e43b3f5ce49d4e749a2784fec9ae
Richard Allen <richard2891@gmail.com>
2019-12-22 22:21:17 -0500

    added get earliest solution.

---
+++ get_earliest/earliest.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+
+def date_sort(date):
+    m, d, y = date.split('/')
+    return (y, m, d)
+
+
+def get_earliest(*dates):
+    return min(dates, key=date_sort)
+
+
+if __name__ == "__main__":
+    print(get_earliest("01/27/1832", "01/27/1756"))
