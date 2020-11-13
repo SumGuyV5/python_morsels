031aea29fc5f5922bddc9ace3caf04bec9098e61
Richard Allen <richard2891@gmail.com>
2020-11-12 23:58:24 -0500

    solution for minmax bonus 3.

--- minmax/minmax.py
+++ minmax/minmax.py
@@ -1,3 +1,11 @@
+class Minmax:
+    def __init__(self, min, max):
+        self.min, self.max = min, max
+    def __iter__(self):
+        yield self.min
+        yield self.max
+
+
 def minmax(num):
     try:
         num = sorted(num)
@@ -6,7 +14,7 @@ def minmax(num):
     except IndexError:
         raise ValueError
 
-    return min, max
+    return Minmax(min, max)
 
 
 if __name__ == '__main__':
