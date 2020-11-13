470c2dd3b66cca42580697b3b507c4c09684790d
Richard Allen <richard2891@gmail.com>
2020-11-12 21:09:57 -0500

    solution for tags_equal.

--- tags_equal/tags_equal.py
+++ tags_equal/tags_equal.py
@@ -1,14 +1,33 @@
+def list_dic(tag1):
+    rtn = {}
+    for tag in tag1:
+        test = tag.split('=')
+        if len(test) > 1:
+            rtn[test[0]] = test[1]
+        else:
+            rtn[test[0]] = ''
+
+    return rtn
+
+def old_parse_tag(html_tag):
+    items = html_tag[1:-1].lower().split()
+    name = items[0]
+    attrs = sorted(items[1:])
+    return name, attrs
+
+def parse_tag(html_tag):
+    items = html_tag[1:-1].lower().split()
+    attributes = [
+        attr.split('=')
+        for attr in reversed(items[1:])
+    ]
+    return items[0], dict(attributes)
 
 def tags_equal(tag1, tag2):
-    tag1 = tag1[1:-1].lower().split()
-    tag2 = tag2[1:-1].lower().split()
+    name1, attrs1 = parse_tag(tag1)
+    name2, attrs2 = parse_tag(tag2)
 
-    for tag in tag1:
-        if tag in tag2:
-            pass
-        else:
-            return False
-    return True
+    return name1 == name2 and attrs1 == attrs2
 
 
 if __name__ == '__main__':
